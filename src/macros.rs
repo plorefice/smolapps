c871ea78e74bb712308660ed2cd2e4a190fd0208
Pietro Lorefice <pietro@develer.com>
2020-05-21 18:25:51 +0200

    macros: fix build with no features

--- src/macros.rs
+++ src/macros.rs
@@ -1,9 +1,10 @@
 //! Utility macros.
 
+#![allow(unused)]
+
 #[cfg(feature = "log")]
 #[macro_use]
 mod log {
-    #[allow(unused)]
     macro_rules! net_log {
         (trace, $($arg:expr),*) => { trace!($($arg),*); };
         (debug, $($arg:expr),*) => { debug!($($arg),*); };
@@ -13,18 +14,15 @@ mod log {
 #[cfg(not(feature = "log"))]
 #[macro_use]
 mod log {
-    #[allow(unused)]
     macro_rules! net_log {
         ($level:ident, $($arg:expr),*) => { $( let _ = $arg; )* }
     }
 }
 
-#[allow(unused)]
 macro_rules! net_trace {
     ($($arg:expr),*) => (net_log!(trace, $($arg),*));
 }
 
-#[allow(unused)]
 macro_rules! net_debug {
     ($($arg:expr),*) => (net_log!(debug, $($arg),*));
 }
